e5acda532ec8e1247f696bd65713ce0f25fe69c2
Trenton Davis <trentondavis2@gmail.com>
2025-04-08 12:20:41 -0400

    fixed conversion file

--- convert-postman.js
+++ convert-postman.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { transpile } = require("postman2openapi");
 
-const inputDir = path.join(__dirname, "postman");
+const inputDir = path.join(__dirname, "postman/collections");
 const outputDir = path.join(__dirname, "docs");
 
 // Ensure the output directory exists
@@ -10,18 +10,13 @@ if (!fs.existsSync(outputDir)) {
   fs.mkdirSync(outputDir);
 }
 
-const files = fs
-  .readdirSync(inputDir)
-  .filter((f) => f.endsWith(".postman_collection.json"));
+const files = fs.readdirSync(inputDir).filter((f) => f.endsWith(".json"));
 
 files.forEach((file) => {
   const collection = require(path.join(inputDir, file));
   const openapi = transpile(collection);
   const jsonOutput = JSON.stringify(openapi, null, 2);
-  const outputFilename = file.replace(
-    ".postman_collection.json",
-    ".openapi.json"
-  );
+  const outputFilename = file.replace(".json", ".openapi.json");
 
   fs.writeFileSync(path.join(outputDir, outputFilename), jsonOutput, "utf8");
   console.log(`Converted ${file} -> ${outputFilename}`);
